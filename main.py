c2b14ad17db60685f657f90e1eada423d854eaa2
Laura Denney <laura.a.denney@gmail.com>
2020-12-02 23:38:56 -0800

    worked on the piglatin assignment

--- main.py
+++ main.py
@@ -14,15 +14,26 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
+
+
+def get_response(fact):
+    response = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/',
+        allow_redirects=False, data={'input_text': fact})
+    return response
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    response = get_response(fact)
+    url = response.headers['Location']
+    body = '<body><a href="{}">Piglatinize this fact please: "{}"</a></body>'\
+    .format(url, fact)
+    # return url
+    return Response(response=body, mimetype='text/html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
